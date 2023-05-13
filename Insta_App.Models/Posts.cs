00d8c943c4b59997ad68ada8d6998db49fba9f31
Sandeep <sandeepsk15892>
2023-05-13 15:33:39 +0530

    code

---
+++ Insta_App.Models/Posts.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Insta_App.Models
+{
+    public class Posts
+    {
+        [Key]
+        public int PostId { get; set; }
+        public int? UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? UserImage { get; set; }
+        public string? PostDescription { get; set; }
+        public string? PostUrl { get; set; }
+        public DateTime? PublishedDate { get; set; }
+    }
+}
